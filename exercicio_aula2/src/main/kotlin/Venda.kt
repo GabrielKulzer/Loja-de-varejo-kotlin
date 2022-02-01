0a56188d56d4d9c3b03ac78dfa8e1b4ac8444ee9
Gabriel Külzer <gabrielk.rodrigues@hotmail.com>
2022-02-01 11:57:37 -0300

    Exercicio aula 2

---
+++ exercicio_aula2/src/main/kotlin/Venda.kt
@@ -0,0 +1,7 @@
+class Venda {
+    var quantidade:Short? = null
+    var valorTotal:Float? = null
+    var vendedor:Vendedor? = null
+    var produto:Produto? = null
+    var cliente:Cliente? = null
+}
