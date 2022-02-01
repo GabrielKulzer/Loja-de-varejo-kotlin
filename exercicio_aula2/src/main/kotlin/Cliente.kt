711a5861eac6e5202329879e7b99b1d885c3f921
Gabriel Külzer <gabrielk.rodrigues@hotmail.com>
2022-02-01 11:55:28 -0300

    Exercicio Loja de varejo aula 2

--- exercicio_aula2/src/main/kotlin/Cliente.kt
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate
-
-class Cliente {
-    var nome:String? = null
-    var cpf:String? = null
-    var endereco:String? = null
-    var dataNascimento:LocalDate? = null
-    var telefoneComercial:String? = null
-}
--- exercicio_aula2/src/main/kotlin/Produto.kt
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate
-
-class Produto {
-    var nome:String? = null
-    var preco:Float? = null
-    var quantidade:Short? = null
-    var validade: LocalDate? = null
-    var fornecedor:Fornecedor? = null
-}
